e912d7fa23f104552dcf0411caea6f4c608e8f61
Colin Faivre <colin.faivre@gmail.com>
2023-08-19 18:15:58 +0200

    fix: :bug: fix wrong Legend type

--- src/game-engine/types.ts
+++ src/game-engine/types.ts
@@ -2,4 +2,4 @@ export type Direction = 'LEFT' | 'RIGHT' | 'UP' | 'DOWN'
 export type Size = { h: number; w: number }
 export type PositionOnSheet = [number, number]
 export type PositionOnMap = [number, number]
-export type Legend = { [index: string]: [number, PositionOnSheet] }
+export type Legend = { [index: string]: PositionOnSheet }
